97efb745f161f2cb6cb554ba2488d2ab91cc2f7e
danik31-h <filippov31012007@gmail.com>
2025-11-10 14:48:38 +0500

    Создание классов. osennikov

---
+++ Regex_Osennikov/Classes/Common/CheckRegex.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions
+
+namespace Regex_Osennikov.Classes.Common
+{
+    public class CheckRegex
+    {
+        public static bool Match(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+    }
+}
---
+++ Regex_Osennikov/Classes/Passport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Osennikov.Classes
+{
+    public class Passport
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Issued {  get; set; }
+        public string DateOfIssued {  get; set; }
+        public string DepartamentCode { get; set; }
+        public string SeriesAndNumber { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PlaceOfBirth {  get; set; }
+    }
+}
